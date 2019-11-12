4150f46dc0498217cc22862000db51eb288aa6e2
sherrycayheyhey <sherrycaywood@yahoo.com>
2019-11-12 10:24:11 -0800

    Initial commit

---
+++ app/src/main/java/com/chromsicle/fontdemo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.chromsicle.fontdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+//change the font family and add the google fonts you want
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
